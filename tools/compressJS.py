4a6193445ca91dc744aa4b4c1d6f815c5e4782cd
ProgrammingNerdGit <gavindistaso@comcast.net>
2024-08-30 18:50:11 -0400

    Fixed JS minifier.

--- tools/compressJS.py
+++ tools/compressJS.py
@@ -2,4 +2,4 @@ import glob
 import os
 
 for file in glob.glob('**/*.js', recursive=True):
-    os.system(f'terser --compress --mangle -- {file}')
+    os.system(f'terser --compress --mangle -o {file} -- {file}')
